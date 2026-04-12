c767e09532fee856128b6482a1e9c1a3ce27be24
CMP2007 <puchecarlos00@gmail.com>
2026-04-12 13:01:14 -0400

    feat: implement Redux Toolkit store and complete exercise 6.10
    Migrated store configuration to configureStore.
    
    Created filterSlice using Redux Toolkit.
    
    Integrated anecdoteReducer and filterReducer into a single store.

--- Part_6/Practices/Redux-notas/src/main.jsx
+++ Part_6/Practices/Redux-notas/src/main.jsx
@@ -1,25 +1,8 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { createStore, combineReducers } from 'redux'
-import { createNote } from './reducers/noteReducer'
-import { filterChange } from './reducers/filterReducer'
-
 import { Provider } from 'react-redux'
 
 import App from './App'
-import noteReducer from './reducers/noteReducer'
-import filterReducer from './reducers/filterReducer'
-
-const reducer = combineReducers({
-  notes: noteReducer,
-  filter: filterReducer
-})
-
-const store = createStore(reducer)
-
-store.subscribe(() => console.log(store.getState()))
-store.dispatch(filterChange('IMPORTANT'))
-store.dispatch(createNote('combineReducers forms one reducer from many simple reducers'))
+import store from './store'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
