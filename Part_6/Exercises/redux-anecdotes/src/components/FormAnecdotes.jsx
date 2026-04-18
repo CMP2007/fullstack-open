48d909e662b14da07eae10e4932bc8c730a3c114
CMP2007 <puchecarlos00@gmail.com>
2026-04-18 09:08:31 -0400

    feat: complete exercise 6.15 - persist new anecdotes to backend
    Refactor AnecdoteForm to save new anecdotes to the server using noteService.

--- Part_6/Exercises/redux-anecdotes/src/components/FormAnecdotes.jsx
+++ Part_6/Exercises/redux-anecdotes/src/components/FormAnecdotes.jsx
@@ -1,16 +1,20 @@
 import { createAnecdote } from '../reducers/anecdoteReducer'
 import { notification, clearNotification } from '../reducers/notificationReducer'
 import { useDispatch } from 'react-redux'
+import noteService from '../services/anecdotes'
 
 const AnecdoteForm = () => {
 
   const dispatch = useDispatch()
 
-  const addAnecdote = (event) => {
+  const addAnecdote = async (event) => {
     event.preventDefault()
     const content = event.target.anecdoteInput.value
     event.target.anecdoteInput.value = ''
-    dispatch(createAnecdote(content))
+    const newAnecdote = await noteService.createNew(content)
+    console.log(newAnecdote);
+    
+    dispatch(createAnecdote(newAnecdote))
     dispatch(notification('Create anecdote'))
     setTimeout(() => {
       dispatch(clearNotification())
