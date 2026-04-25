16cc5f6d11ad9df4fc36ea631228f9cf1282ab6d
CMP2007 <puchecarlos00@gmail.com>
2026-04-25 14:04:18 -0400

    feat: complete exercise 6.24 and server error handling
    - Refactor request.js to extract error messages from the response body.
    - Implement 'onError' in useMutation to capture and display server-side errors.
    - Connect server validation messages to the global notification context.

--- Part_6/Exercises/query-anecdotes/src/components/AnecdoteForm.jsx
+++ Part_6/Exercises/query-anecdotes/src/components/AnecdoteForm.jsx
@@ -15,17 +15,26 @@ const AnecdoteForm = () => {
         payload: `anecdote ${data.content} created`
       })
       queryClient.invalidateQueries({queryKey: ['anecdotes']})
+      setTimeout(()=>{
+        notifiDispatch({ type: ''})
+      }, 3000)
     },
-  })  
+    onError: (error) => {
+      notifiDispatch({ 
+        type: 'error', 
+        payload: error.message 
+      })
+      setTimeout(()=>{
+        notifiDispatch({ type: ''})
+      }, 3000)
+    }
+  })
 
   const onCreate = (event) => {
     event.preventDefault()
     const content = event.target.anecdote.value
     event.target.reset()    
     newAnecdoteMutation.mutate({content, votes: 0})
-    setTimeout(()=>{
-      notifiDispatch({ type: 'notifi', payload: ``})
-    }, 3000)
   }
 
   return (
--- Part_6/Exercises/query-anecdotes/src/notificationContext.jsx
+++ Part_6/Exercises/query-anecdotes/src/notificationContext.jsx
@@ -3,11 +3,9 @@ import { createContext, useReducer } from 'react'
 const notifiReducer = (state, action) => {
   switch (action.type) {
     case 'notifi':
-      console.log(action)
       return action.payload
     case 'error':
-      console.log(action.type);
-      return ''
+      return action.payload
     default:
       return ''
   }
