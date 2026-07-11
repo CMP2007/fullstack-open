a6f3a263ea2d9586694d54f7f88edaa5b86bf89c
CMP2007 <puchecarlos00@gmail.com>
2026-07-11 16:16:01 -0400

    Part 7: fix: use native RegExp wildcard for Express 5 compatibility
    - Replace string wildcard with a native JavaScript Regular Expression to bypass strict path-to-regexp parsing errors during production deployment.

--- Part_7/Exercises/Blog_app/backend/app.js
+++ Part_7/Exercises/Blog_app/backend/app.js
@@ -37,7 +37,7 @@ if (process.env.NODE_ENV === 'test') {
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'dist')))
 
-  app.get('(.*)*', (req, res) => {
+  app.get(/^(?!\/api\/).*$/, (req, res) => {
     res.sendFile(path.join(__dirname, 'dist', 'index.html'))
   })
 }
